da7816de1301a6987add93da976e3ccd11c65442
uspehaleks <uspehaleks@gmail.com>
2026-01-31 13:28:04 +0100

    fix: simplify exports in storage index file\n\n- Changed approach to import already instantiated storage from ../storage\n- Import storage, IStorage and logDiagnostic directly from ../storage\n- Export these elements without trying to instantiate DatabaseStorage again\n- This avoids issues with class instantiation and circular dependencies

--- server/storage/index.ts
+++ server/storage/index.ts
@@ -1,8 +1,4 @@
 // server/storage/index.ts
-import { DatabaseStorage } from "../storage";
+import { storage, IStorage, logDiagnostic } from "../storage";
 
-export const storage = new DatabaseStorage();
-
-export { IStorage } from "../storage";
-export { storage };
-export { logDiagnostic } from "../storage";
+export { storage, IStorage, logDiagnostic };
